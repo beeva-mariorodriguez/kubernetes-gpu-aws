8ab64e2a8a4e6e997cd4700713bd8db60ec20220
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-11-15 12:21:49 +0100

    CoreOS image

--- createcluster.sh
+++ createcluster.sh
@@ -11,7 +11,7 @@ kops create cluster \
     --state "$(terraform output kops_state_store)" \
     --zones "$(terraform output -json aws_azs | jq -r '.value|join(",")')" \
     --kubernetes-version "1.8.3" \
-    --image "$(terraform output coreos_image_location)" \
+    --image "aws-marketplace/CoreOS-stable-1520.8.0-hvm-0d1e0bd0-eaea-4397-9a3a-c56f861d2a14-ami-a89d3ad2.4" \
     --node-size "t2.medium" \
     --network-cidr "$(terraform output aws_cidr_block)" \
     --dns-zone "$(terraform output aws_dns_zone_id)" \
